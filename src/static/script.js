45bdbdad5466676d7717e76d5054a391fb768410
Mike Lei <mikelei8291@users.noreply.github.com>
2024-12-30 03:11:56 +0800

    refactor(webview): optimize drag and drop placeholder display
    Optimize drag and drop positioning algorithm.

--- src/static/script.js
+++ src/static/script.js
@@ -1,51 +1,58 @@
 import { TodoList } from "./components/todo-list/index.js";
-
-const ITEM_HALF_HEIGHT = 11;
-const ITEM_GAP = ITEM_HALF_HEIGHT * 2 + 8;
+import { createElement } from "./components/utils.js";
 
 const vscode = acquireVsCodeApi();
 const todoList = new TodoList(vscode);
 todoList.id = "todo-list";
 todoList.addEventListener("state-change", ({ detail }) => (detail?.save ?? true) ? vscode.postMessage(todoList.toArray()) : undefined);
 
-let dropTarget;
+const dropTarget = createElement("div", { id: "drop-target" });
+let previousTarget = undefined;
 todoList.addEventListener("dragover", (event) => {
     event.preventDefault();
-    let target = Array.from(todoList.querySelectorAll("li:not(.dragging)")).map(element => {
-        const { y } = element.getBoundingClientRect();
-        return { offset: event.clientY - (y + ITEM_HALF_HEIGHT), element };
-    }).reduce(
-        (closest, item) => item.offset >= 0 && item.offset <= ITEM_GAP && item.offset < closest.offset ? item : closest,
-        { offset: Infinity }
-    ).element;
-    if (!target && event.clientY < ITEM_HALF_HEIGHT) {
-        target = todoList;
-    }
-    if (target !== dropTarget) {
-        dropTarget?.classList?.remove("drop-target");
-        target?.classList?.add("drop-target");
-        dropTarget = target;
+    if (event.target.matches("#todo-list, div:not(#drop-target), div:not(#drop-target) *")) {
+        const items = Array.from(todoList.querySelectorAll("li:not(.dragging) > div"));
+        const maxHeight = Math.max(...items.map(item => item.getBoundingClientRect().bottom));
+        const target = items.reduce(
+            (closest, element) => {
+                const { y, height } = element.getBoundingClientRect();
+                const offset = event.clientY - (y + height / 2);
+                return event.clientY >= 11
+                    && event.clientY <= maxHeight + 22
+                    && offset > 0
+                    && offset <= height / 2 + 8
+                    && offset < closest.offset
+                    ? { offset, element } : closest;
+            },
+            { offset: Infinity }
+        ).element?.parentElement ?? (event.clientY < 11 ? "top" : event.clientY > maxHeight + 22 ? "bottom" : undefined);
+        if (target !== previousTarget) {
+            previousTarget = target;
+            if (target === "top") {
+                todoList.prepend(dropTarget);
+            } else if (target === "bottom") {
+                todoList.appendChild(dropTarget);
+            } else if (target?.subList) {
+                target.subList.prepend(dropTarget);
+            } else if (target) {
+                target.after(dropTarget);
+            }
+        }
     }
 });
 todoList.addEventListener("drop", (event) => {
     const item = document.getElementById(event.dataTransfer.getData("text/plain"));
     const parent = item.parentElement;
-    if (dropTarget) {
-        if (dropTarget === todoList) {
-            todoList.prepend(item);
-        } else if (dropTarget.subList) {
-            dropTarget.subList.prepend(item);
-        } else {
-            dropTarget.after(item);
-        }
-        dropTarget.classList.remove("drop-target");
-    } else {
-        todoList.append(item);
-    }
+    dropTarget.replaceWith(item);
     if (item.parentElement !== parent) {
         parent.dispatchEvent(new CustomEvent("state-change", { bubbles: true, detail: { save: false } }));
     }
     item.dispatchEvent(new CustomEvent("state-change", { bubbles: true }));
+});
+todoList.addEventListener("dragend", () => {
+    if (document.body.contains(dropTarget)) {
+        dropTarget.remove();
+    }
 });
 
 document.body.appendChild(todoList);
